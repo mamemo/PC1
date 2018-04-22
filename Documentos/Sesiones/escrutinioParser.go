9b79087ccda8ed59495865a117225e43819cbada
Mauro <mauromm1996@gmail.com>
2018-04-21 23:24:06 -0600

    Segunda Ronda de Elecciones

--- Documentos/Sesiones/escrutinioParser.go
+++ Documentos/Sesiones/escrutinioParser.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 
-	for i := 1; i < 13; i++ {
-		excelFileName := fmt.Sprintf("./actas/ActaSesion%d.xlsx",i)
+	for i := 1; i < 9; i++ {
+		excelFileName := fmt.Sprintf("./actas2/ActaSesion%d.xlsx",i)
 		xlFile, err := xlsx.OpenFile(excelFileName)
 		if err != nil {
 			log.Println(err)
@@ -23,15 +23,27 @@ func main() {
 func parseAct(file *xlsx.File){
     for _, sheet := range file.Sheets {
 		for i := 1; i < len(sheet.Rows[2].Cells); i++ {
-			for j := 2; j < 20; j++ {
-				if j==2 || j==19{
-				k,_:= sheet.Rows[j].Cells[i].Int()
-				if j == 2 {
-					fmt.Printf("%d,",k)
-				}else{
-					fmt.Printf("%d",k)
-				}}
-			}
+			junta,_:= sheet.Rows[2].Cells[i].Int()
+			pac,_:= sheet.Rows[3].Cells[i].Int()
+			rn,_:= sheet.Rows[4].Cells[i].Int()
+			nulos,_:= sheet.Rows[6].Cells[i].Int()
+			blancos,_:= sheet.Rows[7].Cells[i].Int()
+			votos,_:= sheet.Rows[8].Cells[i].Int()
+			fmt.Printf("%d,",junta)
+			fmt.Printf("%d,",pac)
+			fmt.Printf("%d,",rn)
+			fmt.Printf("%d,",nulos)
+			fmt.Printf("%d,",blancos)
+			fmt.Printf("%d",votos)
+			// for j := 2; j < 11; j++ {
+			// 	if j==2 || j==19{
+			// 	k,_:= sheet.Rows[j].Cells[i].Int()
+			// 	if j == 2 {
+			// 		fmt.Printf("%d,",k)
+			// 	}else{
+			// 		fmt.Printf("%d",k)
+			// 	}}
+			// }
 			fmt.Printf("\n")
 		}
 	}
