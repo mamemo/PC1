36287f351b1eacf2407cf36d02eff60e631659dd
valebm <valebm18@gmail.com>
2018-04-23 18:25:36 -0600

    Corrige lista de indices de generador.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tec.ic.ia.pc1.g07',
-      version='1.1.1',
+      version='1.2.0',
       py_modules=['tec.ic','tec.ic.ia','tec.ic.ia.pc1','tec.ic.ia.pc1.g07'],
       description='Generador de muestras basados en los resultados electorales presidenciales de la primera ronda en Costa Rica.',
       author='Trifuerza',
